1b0274914b1cee58e857b033d047d931f013c63a
taqtaq11 <taqtaq11@gmail.com>
2016-01-11 23:09:05 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,154 +1,253 @@
 #define FUSE_USE_VERSION 26
 
 #include <fuse.h>
-#include <stdio.h>
-#include <string>
 #include <errno.h>
+#include <stdio.h>
 #include <fcntl.h>
-
 #include <stdlib.h>
 #include <string.h>
 
-using namespace std;
+#define DATA_BLOCK_SIZE 2048
+#define MAX_DATABLOCKS_COUNT 1024000
+#define MAX_FILES_COUNT 32
+#define FILENAME_LENGTH 128
+#define PATH_TO_LLFSFILE "/home/taqtaq11/ClionProjects/LLFileSystem/fs"
+#define INITIAL_EMPTY_NUM -2
 
-const int DATA_BLOCK_SIZE = 2048;
-const int MAX_DATABLOCKS_COUNT = 1024;
-const int MAX_FILES_COUNT = 32;
-const char* PATH_TO_LLFSFILE = "/home/taqtaq11/ClionProjects/LLFileSystem/fs";
-
-struct fileDescriptor {
-    bool isFolder;
-    bool isEmpty;
+typedef struct fileDescriptor {
+    char name[FILENAME_LENGTH];
+    int isFolder;
+    int isFill;
     int startingDataBlock;
-    size_t dataSize;
-    string* name;
-};
+    int dataSize;
+} fileDescriptor;
 
-struct dataBlock {
-    const char* data;
-    int next;
-};
-
-bool isEmptyBlocks[MAX_DATABLOCKS_COUNT];
+int nextBlockDict[MAX_DATABLOCKS_COUNT];
 fileDescriptor fds[MAX_FILES_COUNT];
 
-int getEmptyFileDescriptorNum() {
-    for (int i = 0; i < MAX_FILES_COUNT; ++i) {
-        if (fds[i].isEmpty) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-int getEmptyDataBlockNum() {
-    for (int i = 0; i < MAX_DATABLOCKS_COUNT; ++i) {
-        if (isEmptyBlocks[i]) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
 void init() {
-    FILE* llfsFile = fopen(PATH_TO_LLFSFILE, "r");
+    int i = 0;
 
-    fread(fds, sizeof(fileDescriptor), MAX_FILES_COUNT, llfsFile);
-    fread(isEmptyBlocks, sizeof(bool), MAX_DATABLOCKS_COUNT, llfsFile);
+    int num = sizeof(fds) / sizeof(fds[0]);
+    while (i < num) {
+        memset(fds[i].name, '\0', FILENAME_LENGTH);
+        fds[i].isFolder = 0;
+        fds[i].isFill = 0;
+        fds[i].startingDataBlock = -1;
+        fds[i].dataSize = 0;
+        i++;
+    }
 
-    fclose(llfsFile);
+    int j = 0;
+    while (j < MAX_DATABLOCKS_COUNT) {
+        nextBlockDict[j] = INITIAL_EMPTY_NUM;
+        j++;
+    }
 }
 
-void createNewFS() {
-    FILE* llfsFile = fopen(PATH_TO_LLFSFILE, "w");
-
-    fileDescriptor* buf = (fileDescriptor*)malloc(sizeof(fileDescriptor));
-    for (int i = 0; i < MAX_FILES_COUNT; ++i) {
-        buf->name = new string();
-        buf->isEmpty = true;
-        fwrite(buf, sizeof(fileDescriptor), 1, llfsFile);
-    }
-    free(buf);
-
-    bool* bufBool = (bool *)malloc(sizeof(bool));
-    for (int i = 0; i < MAX_DATABLOCKS_COUNT; ++i) {
-        fwrite(bufBool, sizeof(bool), 1, llfsFile);
-    }
-    free(bufBool);
-
-    dataBlock* bufInt = (dataBlock*)malloc(sizeof(dataBlock));
-    for (int i = 0; i < MAX_DATABLOCKS_COUNT; ++i) {
-        fwrite(bufInt, sizeof(dataBlock), 1, llfsFile);
-    }
-    free(bufInt);
-
-    fclose(llfsFile);
-}
-
-int getFileDescriptor(const char* name) {
-    for (int i = 0; i < MAX_FILES_COUNT; ++i) {
-        if (fds[i].name->c_str() == name) {
+int getEmptyFD() {
+    int i = 0;
+    int n = sizeof(fds) / sizeof(fds[0]);
+    while (i < n) {
+        if (!fds[i].isFill)
             return i;
-        }
+        i++;
     }
-
     return -1;
 }
 
-void getData(int blockNum, char* data, int handledBlocksNum, FILE* file) {
-    size_t offset = sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(bool) * MAX_DATABLOCKS_COUNT
-                    + sizeof(dataBlock) * (blockNum - 1);
-
-    dataBlock* db = NULL;
-    fseek(file, offset, SEEK_SET);
-    fread(db, sizeof(dataBlock), 1, file);
-
-    char* buf = (data + handledBlocksNum * DATA_BLOCK_SIZE);
-    *buf = *(db->data);
-
-    if (db->next != -1) {
-        getData(db->next, data, handledBlocksNum + 1, file);
+int getEmptyDataBlock() {
+    int i = 0;
+    while (i < MAX_DATABLOCKS_COUNT) {
+        if (nextBlockDict[i] == INITIAL_EMPTY_NUM)
+            return i;
+        i++;
     }
+    return -1;
 }
 
-void writeFileDescriptor(fileDescriptor* fd, int position, FILE* file) {
-    fseek(file, (position - 1) * sizeof(fileDescriptor), SEEK_SET);
-    fwrite(fd, sizeof(fileDescriptor), 1, file);
+int writeFileDescriptor(int num) {
+    FILE *f = fopen(PATH_TO_LLFSFILE, "r+");
+    fseek(f, num * sizeof(fileDescriptor), SEEK_SET);
+    fwrite(&fds[num], sizeof(fileDescriptor), 1, f);
+    fclose(f);
+    return 0;
 }
 
-void writeDataBlock(int blockNum, const char* data, FILE* file, size_t cnt, size_t size) {
-    size_t offset = sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(bool) * MAX_DATABLOCKS_COUNT
-                    + sizeof(dataBlock) * (blockNum - 1);
-    dataBlock* block = (dataBlock*)malloc(sizeof(dataBlock));
-    block->data = data + (size - cnt) / DATA_BLOCK_SIZE;
-    block->next = -1;
+int writeDataBlocks(fileDescriptor* fd) {
+    int i = fd->startingDataBlock;
+    FILE *f = fopen(PATH_TO_LLFSFILE, "r+");
 
-    cnt -= DATA_BLOCK_SIZE;
-
-    int nBlockNum;
-    if (cnt > 0) {
-        nBlockNum = getEmptyDataBlockNum();
-        isEmptyBlocks[nBlockNum] = false;
-        block->next = nBlockNum;
+    while (i != -1) {
+        fseek(f, sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(int) * i, SEEK_SET);
+        fwrite(&nextBlockDict[i], sizeof(int), 1, f);
+        i = nextBlockDict[i];
     }
 
-    fseek(file, offset, SEEK_SET);
-    fwrite(block, sizeof(dataBlock), 1, file);
-
-    if (cnt > 0) {
-        writeDataBlock(nBlockNum, data, file, cnt, size);
-    }
-
-    free(block);
+    fclose(f);
+    return 0;
 }
 
-char* getDirName(char* path) {
+
+int writeData(fileDescriptor* fd, const char* data, int size, int offset) {
+    if (size == 0) return 0;
+    FILE *f = fopen(PATH_TO_LLFSFILE, "r+");
+
+    int i = fd->startingDataBlock, j = offset / DATA_BLOCK_SIZE;
+    while (j-- > 0) i = nextBlockDict[i];
+
+    int count = 0;
+    int recalcOffset = offset % DATA_BLOCK_SIZE;
+    int border = size;
+
+    int headers_offset = sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(int) * MAX_DATABLOCKS_COUNT;
+
+    while (border > 0) {
+        if (recalcOffset + border > DATA_BLOCK_SIZE)
+            count = DATA_BLOCK_SIZE - recalcOffset;
+        else
+            count = border;
+
+        fseek(f, headers_offset + i * DATA_BLOCK_SIZE + recalcOffset, SEEK_SET);
+        fwrite(data + size - border, 1, count, f);
+
+        if (recalcOffset + count == DATA_BLOCK_SIZE) {
+            if (nextBlockDict[i] >= 0) {
+                i = nextBlockDict[i];
+            }
+            else {
+                int fdNum = getEmptyDataBlock();
+                nextBlockDict[i] = fdNum;
+                nextBlockDict[fdNum] = -1;
+                i = fdNum;
+            }
+        }
+
+        border = border - count;
+        recalcOffset = 0;
+    }
+
+    fd->dataSize = size + offset;
+
+    writeDataBlocks(fd);
+    fclose(f);
+    return size;
+}
+
+int readData(fileDescriptor* fd, char** data) {
+    if (fd == NULL) return -1;
+    FILE *f = fopen(PATH_TO_LLFSFILE, "r");
+    char *buf = NULL;
+    int size = fd->dataSize;
+    buf = (char *)malloc(size);
+
+    int i = fd->startingDataBlock;
+    int j = 0;
+    int count;
+
+    int headers_offset = sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(int) * MAX_DATABLOCKS_COUNT;
+    while (i != -1) {
+        int left = size - j * DATA_BLOCK_SIZE;
+        if (left >= DATA_BLOCK_SIZE)
+            count = DATA_BLOCK_SIZE;
+        else
+            count = left;
+        fseek(f, headers_offset + i * DATA_BLOCK_SIZE, SEEK_SET);
+        fread(buf + j * DATA_BLOCK_SIZE, 1, count, f);
+        i = nextBlockDict[i];
+        j++;
+    }
+    fclose(f);
+    *data = buf;
+    return size;
+
+}
+
+int getFDNum(char* data, char* name, int size) {
+    int i = 0;
+    int n = size / sizeof(int);
+    while (i < n) {
+        if (strcmp(fds[((int *)data)[i]].name, name) == 0)
+            return ((int *)data)[i];
+        i++;
+    }
+    return -1;
+}
+
+int getFileDescriptor(const char* path, fileDescriptor** fd) {
+    char *fpath = (char*)malloc(strlen(path));
+    strcpy(fpath, path);
+
+    if (fpath && strcmp("/", fpath) == 0) {
+        *fd = &fds[0];
+        return 0;
+    }
+
+    fileDescriptor *m = NULL;
+    char *p;
+
+    p = fpath;
+    if (*p++ == '/')
+        m = &fds[0];
+    else return -1;
+    char *data, *s;
+    char name[FILENAME_LENGTH];
+    memset(name, '\0', FILENAME_LENGTH);
+
+    int fdNum = -1, size;
+    while (p - fpath < strlen(fpath)) {
+        if (m->dataSize == 0)
+            return -1;
+        size = readData(m, &data);
+        s = p;
+        p = strchr(p, '/');
+        if (p != NULL) {
+            p = p + 1;
+            strncpy(name, s, p - s - 1);
+        }
+        else {
+            strncpy(name, s, fpath + strlen(fpath) - s);
+            p = fpath + strlen(fpath);
+        }
+        fdNum = getFDNum(data, name, size);
+        if (fdNum == -1) return -1;
+        m = &fds[fdNum];
+        memset(name, '\0', FILENAME_LENGTH);
+        free(data);
+    }
+
+    *fd = m;
+    return fdNum;
+}
+
+int addFile(char* name, int size, int isFolder) {
+    fileDescriptor *fd = NULL;
+    int fd_num = getEmptyFD();
+    if (fd_num == -1) return -1;
+    fd = &fds[fd_num];
+    int start = getEmptyDataBlock();
+    if (start == -1) return -1;
+
+    strcpy(fd->name, name);
+    fd->startingDataBlock = start;
+    fd->dataSize = size;
+    fd->isFolder = isFolder;
+    fd->isFill = 1;
+
+    nextBlockDict[start] = -1;
+    writeFileDescriptor(fd_num);
+    writeDataBlocks(fd);
+    return fd_num;
+}
+
+char* getDirName(const char* path) {
     char* directory;
     char* p = NULL;
-    char* ptr = path;
+    char* ptr = (char*)malloc(sizeof(path));
+    strcpy(ptr, path);
+
     while (p = *ptr == '/' ? ptr : p, *ptr++ != '\0');
+
     if ((p - path) != 0) {
         directory = (char*)malloc(sizeof(char) * (p - path));
         strncpy(directory, path, p - path);
@@ -161,24 +260,80 @@ char* getDirName(char* path) {
     return directory;
 }
 
-char* getFileName(char *path) {
-    char* filename;
-    char* p = NULL;
-    char *ptr = path;
-    while (p = *ptr == '/' ? ptr : p, *ptr++ != '\0');
-    filename = (char*)malloc(sizeof(char) * (ptr - p));
-    strncpy(filename, p + 1, ptr - p);
-    return filename;
+int deleteFile(const char* path) {
+    fileDescriptor *fileNode, *dirNode;
+    char* data;
+    char* preparedData;
+    char *dir = getDirName(path);
+
+    int dirNodeNum = getFileDescriptor(dir, &dirNode);
+    int fNodeNum = getFileDescriptor(path, &fileNode);
+    int size = readData(dirNode, &data);
+
+    preparedData = (char *)malloc(size - sizeof(int));
+    int i = 0, j = 0;
+    while (i < size / sizeof(int)) {
+        if (((int *)data)[i] != fNodeNum)
+            ((int *) preparedData)[j++] = ((int *)data)[i];
+        i++;
+    }
+
+    writeData(dirNode, preparedData, size, 0);
+    dirNode->dataSize = size - sizeof(int);
+    writeFileDescriptor(dirNodeNum);
+
+    free(data);
+    free(dir);
+
+    return 0;
 }
 
-static int llGetattr(const char* path, struct stat *stbuf) {
-    int fdNum = getFileDescriptor(path);
-    if (fdNum == -1)
-        return -ENOENT;
+static void* llInit(struct fuse_conn_info* fi) {
+    FILE* llfsFile = fopen(PATH_TO_LLFSFILE, "w+");
 
-    fileDescriptor* fd = &fds[fdNum];
-    printf("aaa\n");
-    if (fd->isFolder) {
+    char* buf = (char*)malloc(sizeof(fileDescriptor));
+    memset(buf, '\0', sizeof(fileDescriptor));
+    int i = 0;
+    while (i < MAX_FILES_COUNT) {
+        fwrite(buf, sizeof(fileDescriptor), 1, llfsFile);
+        i++;
+    }
+    free(buf);
+
+    buf = (char*)malloc(sizeof(int));
+    memset(buf, '\0', sizeof(int));
+    i = 0;
+    while (i < MAX_DATABLOCKS_COUNT) {
+        fwrite(buf, sizeof(int), 1, llfsFile);
+        i++;
+    }
+    free(buf);
+
+    buf = (char*)malloc(DATA_BLOCK_SIZE);
+    memset(buf, '\0', DATA_BLOCK_SIZE);
+    i = 0;
+    while (i < MAX_DATABLOCKS_COUNT) {
+        fwrite(buf, DATA_BLOCK_SIZE, 1, llfsFile);
+        i++;
+    }
+    free(buf);
+
+    init();
+    addFile("/", 0, 1);
+    fclose(llfsFile);
+    return 0;
+}
+
+static int llGetattr(const char* path, struct stat* stbuf) {
+    int res = 0;
+
+    fileDescriptor *fd;
+    if (getFileDescriptor(path, &fd) == -1)
+        res = -ENOENT;
+
+    memset(stbuf, 0, sizeof(struct stat));
+
+    if(fd->isFolder) {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
     }
@@ -187,98 +342,187 @@ static int llGetattr(const char* path, struct stat *stbuf) {
         stbuf->st_nlink = 1;
         stbuf->st_size = fd->dataSize;
     }
-
     stbuf->st_mode = stbuf->st_mode | 0777;
+
+    return res;
+}
+
+static int llCreate(const char* path, mode_t mode, struct fuse_file_info* finfo) {
+    fileDescriptor *fd;
+    char* dir;
+    char* name;
+    char* data;
+
+    name = strrchr(path, '/');
+    if (name == NULL) {
+        strcpy(name, path);
+        dir = (char *)malloc(2);
+    } else {
+        name++;
+        int len = strlen(path) - strlen(name) ;
+        dir = (char *)malloc(len + 1);
+        strncpy(dir, path, len);
+        dir[len] = '\0';
+    }
+    int nodeNum = getFileDescriptor(dir, &fd);
+    int size = readData(fd, &data);
+
+    char* preparedData = (char*)malloc(size + sizeof(int));
+    memcpy(preparedData, data, size);
+
+    int fdNum = addFile(name, 0, 0);
+    ((int*) preparedData)[size/sizeof(int)] = fdNum;
+
+    writeData(fd, preparedData, size + sizeof(int), 0);
+    fd->dataSize = size + sizeof(int);
+    writeFileDescriptor(nodeNum);
+
+    free(preparedData);
+    free(dir);
     return 0;
 }
 
-static int llReaddir(const char* path, void* buf, fuse_fill_dir_t filler,
-                     off_t offset, struct fuse_file_info* fi) {
-    printf("check readdir");
+static int llReaddir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
+
+    fileDescriptor *fd;
+    int nodeNum = getFileDescriptor(path, &fd);
+    if (nodeNum == -1) return -ENOENT;
+
+    char *data;
+    int size = readData(fd, &data);
+
+    int i = 0, n = size/sizeof(int);
+    while (i < n) {
+        filler(buf, fds[((int*)data)[i]].name, NULL, 0);
+        i++;
+    }
+
     return 0;
 }
 
-static int llOpen(const char* path, fuse_file_info* fi) {
-    printf("check open");
-    int fd = getFileDescriptor(path);
-    if (fd == -1)
-        return -ENOENT;
-    return 0;
-}
+static int llRead(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* fi) {
 
-static int llRead(const char* path, char* buf, size_t size, off_t offset,
-                    fuse_file_info* fi) {
-    FILE* llfsFile = fopen(PATH_TO_LLFSFILE, "r");
-    fileDescriptor* fd = &fds[getFileDescriptor(path)];
-    char* data = (char*)malloc(sizeof(char) * (offset + size));
-    getData(fd->startingDataBlock, data, 0, llfsFile);
-    memcpy(buf, data + offset, size);
-    fclose(llfsFile);
-    free(data);
-    return size;
+    if (size == 0) return 0;
+
+    fileDescriptor *fd;
+    int nodeNum = getFileDescriptor(path, &fd);
+    if (nodeNum == -1) return -ENOENT;
+
+    int s = fd->dataSize;
+    if (offset > s) return 0;
+    if (size < s) s = size;
+
+    FILE *f = fopen(PATH_TO_LLFSFILE, "r");
+
+    int i = fd->startingDataBlock, j = offset / DATA_BLOCK_SIZE;
+    while (j-- > 0) i = nextBlockDict[i];
+
+    int left = s;
+    int count = 0;
+    int off = offset % DATA_BLOCK_SIZE;
+
+    char *data = (char *)malloc(s);
+
+    int headers_offset = sizeof(fileDescriptor) * MAX_FILES_COUNT + sizeof(int) * MAX_DATABLOCKS_COUNT;
+
+    while (left > 0) {
+        if (off + left > DATA_BLOCK_SIZE)
+            count = DATA_BLOCK_SIZE - off;
+        else
+            count = left;
+
+        fseek(f, headers_offset + i * DATA_BLOCK_SIZE + off, SEEK_SET);
+        fread(data + s - left, 1, count, f);
+
+        if (off + count == DATA_BLOCK_SIZE) {
+            i = nextBlockDict[i];
+        }
+
+        left = left - count;
+        off = 0;
+    }
+
+    memcpy(buf, data, s);
+    fclose(f);
+    return s;
 }
 
 static int llMkdir(const char* path, mode_t mode) {
-    int fdNum = getEmptyFileDescriptorNum();
-    fds[fdNum].name = new string(path);
-    fds[fdNum].isEmpty = false;
-    fds[fdNum].isFolder = true;
-    return 0;
-}
+    fileDescriptor *fd;
+    char* dir;
+    char* name;
+    char* data;
 
-static int llWrite(const char* path, const char* buf, size_t size, off_t offset,
-                    fuse_file_info* fi) {
-    printf("writing\n");
-    fileDescriptor* fd;
-    int fdNum = getFileDescriptor(path);
-    if (fdNum == -1) {
-        fdNum = getEmptyFileDescriptorNum();
+    name = strrchr(path, '/');
+    if (name == NULL) {
+        strcpy(name, path);
+        dir = (char *)malloc(2);
+    } else {
+        name++;
+        int len = strlen(path) - strlen(name) ;
+        dir = (char *)malloc(len + 1);
+        strncpy(dir, path, len);
+        dir[len] = '\0';
     }
-    fd = &fds[fdNum];
-    int emptyBlockNum = getEmptyDataBlockNum();
-    isEmptyBlocks[emptyBlockNum] = false;
-    fd->isEmpty = false;
-    fd->name = new string(path);
-    fd->startingDataBlock = emptyBlockNum;
-    fd->dataSize = size;
+    int nodeNum = getFileDescriptor(dir, &fd);
+    int size = readData(fd, &data);
 
-    FILE* llfsFile = fopen(PATH_TO_LLFSFILE, "w");
-    writeFileDescriptor(fd, fdNum, llfsFile);
-    writeDataBlock(emptyBlockNum, buf, llfsFile, size, size);
-    fclose(llfsFile);
+    char* preparedData = (char*)malloc(size + sizeof(int));
+    memcpy(preparedData, data, size);
 
+    int fdNum = addFile(name, 0, 1);
+    ((int*) preparedData)[size/sizeof(int)] = fdNum;
+
+    writeData(fd, preparedData, size + sizeof(int), 0);
+    fd->dataSize = size + sizeof(int);
+    writeFileDescriptor(nodeNum);
+
+    free(preparedData);
+    free(dir);
     return 0;
 }
 
-static int llCreate(const char* path, mode_t mode, fuse_file_info* fi) {
-    printf("creating");
-    int fdNum = getEmptyFileDescriptorNum();
-    fds[fdNum].name = new string(path);
-    fds[fdNum].isEmpty = false;
-    fds[fdNum].isFolder = false;
+static int llOpen(const char* path, struct fuse_file_info* fi)
+{
+    fileDescriptor *fd;
+    getFileDescriptor(path, &fd);
+    //TODO
     return 0;
 }
 
-static int llOpendir(const char *path, fuse_file_info *fi) {
-
+static int llWrite(const char* path, const char* buf, size_t nbytes, off_t offset, struct fuse_file_info* fi) {
+    fileDescriptor *fd;
+    int nodeNum = getFileDescriptor(path, &fd);
+    if (nodeNum == -1) return -ENOENT;
+    int s = writeData(fd, buf, nbytes, offset);
+    if (s != -1) {
+        writeFileDescriptor(nodeNum);
+        return s;
+    }
+    return -1;
 }
 
-int main(int argc, char *argv[]) {
-    printf("before\n");
-    createNewFS();
-    init();
-    printf("after\n");
+static int llUnlink(const char* path) {
+    int res = deleteFile(path);
+    if (res != 0) return -1;
+    return 0;
+}
 
-    fuse_operations* oper = (fuse_operations*)malloc(sizeof(fuse_operations));
-    oper->getattr = &llGetattr;
-    oper->readdir = &llReaddir;
-    oper->open = &llOpen;
-    oper->read = &llRead;
-    oper->mkdir = &llMkdir;
-    oper->write = &llWrite;
-    oper->create = &llCreate;
+static struct fuse_operations fs_oper = {
+        .init		= llInit,
+        .getattr	= llGetattr,
+        .readdir	= llReaddir,
+        .create		= llCreate,
+        .open		= llOpen,
+        .read		= llRead,
+        .write		= llWrite,
+        .unlink		= llUnlink,
+        .mkdir      = llMkdir,
+};
 
-    return fuse_main(argc, argv, oper, NULL);
+int main(int argc, char *argv[])
+{
+    return fuse_main(argc, argv, &fs_oper, NULL);
 }
